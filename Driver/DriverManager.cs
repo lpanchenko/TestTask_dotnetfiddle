0e12509ad89e3b09241cc63d97347f1cc9cd1502
lpanchenko <liuba.panchenko@gmail.com>
2020-12-22 20:56:32 -0500

    Adding Selenium + helper classes

---
+++ Driver/DriverManager.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium.Driver
+{
+    public static class DriverManager
+    {
+        public static void CloseDriver(IWebDriver driver)
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
