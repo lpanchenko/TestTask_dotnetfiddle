b7d91a9b197d5d1a112c9fc3b3990992a976298b
lpanchenko <liuba.panchenko@gmail.com>
2020-12-22 21:10:11 -0500

    Finish "Click Hide Options -> Check That Panel is Hidden" test

--- Tests.cs
+++ Tests.cs
@@ -20,5 +20,21 @@ namespace Selenium
                             homePage.GetOutputValue(),
                             $"Output should contains value {expectedOutput}");
         }
+
+        [Test]
+        public void ClickHideOptions_CheckThatPanelHided()
+        {
+            // Arrange
+            var homePage = Pages.Home.Open();
+            Assert.IsTrue(homePage.OptionsIsVisible(),
+                "Options should be visible");
+
+            // Act
+            homePage.HideOptions();
+
+            // Assert
+            Assert.IsFalse(homePage.OptionsIsVisible(),
+                           "The options should be hided");
+        }
     }
 }
